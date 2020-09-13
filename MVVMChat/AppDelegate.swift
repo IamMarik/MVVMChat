2e98887952dd82ae7f2cb8b4dd1402176077de16
Marat Dzhanybaev <maratkind@gmail.com>
2020-09-13 14:11:28 +1000

    Create project.

---
+++ MVVMChat/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  MVVMChat
+//
+//  Created by Marik on 13.09.2020.
+//  Copyright © 2020 Marat. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '11.0'
+
+target 'MVVMChat' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for MVVMChat
+
+  target 'MVVMChatTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'MVVMChatUITests' do
+    # Pods for testing
+  end
+
+end
